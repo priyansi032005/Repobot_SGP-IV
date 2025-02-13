11d4e16dc657132fbf5b88c38adeb6d64bf388e1
MelitaCastelino13 <22cs013@charusat.edu.in>
2025-02-13 10:06:29 +0530

    Contact Changes

--- Frontend/src/pages/Home/Contact.jsx
+++ Frontend/src/pages/Home/Contact.jsx
@@ -113,8 +113,8 @@ const ContactPage = () => {
                                     <h3 className="font-semibold text-[#441752]">Address</h3>
                                     <p className="text-gray-600">
                                         123 Tech Street<br />
-                                        San Francisco, CA 94105<br />
-                                        United States
+                                        Rajkot, Gujarat<br />
+                                        India
                                     </p>
                                 </div>
                             </div>
